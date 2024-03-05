f522fa7996e287e5b514a4999ddb7a7ec4404fd8
jordan <sudo.sendmail.jordan@gmail.com>
2024-03-05 05:14:33 -0800

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,31 @@ import (
 	"os"
 )
 
+
 func main() {
     arguments := os.Args
-    fmt.Println(arguments)
+	if len(arguments) == 0{
+		fmt.Println("No arguments error")
+		return
+	}
+	if len(arguments) == 1{
+		fmt.Println("Missing arguments error")
+		return
+	}
+	if len(arguments) == 2{
+		fmt.Println("Missing arguments error")
+		return
+	}
+	if len(arguments) == 3{
+		fmt.Println("Missing arguments error")
+		return
+	}
+	if len(arguments) == 4{
+		fmt.Println(arguments[0])
+		fmt.Println(arguments[1])
+		fmt.Println(arguments[2])
+		fmt.Println(arguments[3])
+		return
+	}
 }
+
