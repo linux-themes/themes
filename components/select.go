70a5f16650033db0b646cf49de460d75d9972848
jordan <sudo.sendmail.jordan@gmail.com>
2024-03-08 07:13:42 -0800

    Add: Multiselect

---
+++ components/select.go
@@ -0,0 +1,140 @@
+package components
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/huh/spinner"
+	"github.com/charmbracelet/lipgloss"
+)
+
+// PATHS
+func SetUser() string {
+	shell_variables := os.Environ()
+	for _, variable := range shell_variables {
+		if strings.Contains(variable, "LOGNAME=") {
+			strings := strings.Split(variable, "LOGNAME=")
+			return strings[1]
+		}
+	}
+	return "User Error"
+}
+
+var USER = SetUser()
+var HOME_PATH = "/home"
+var USER_PATH = HOME_PATH + "/" + USER
+var ICON_PATH = USER_PATH + "/.icons"
+var CONFIG_PATH = USER_PATH + "/.config"
+var THEME_PATH = USER_PATH + "/.themes"
+var TERMINAL_PATH = USER_PATH + "/.gnome/terminal/themes"
+var ULAUNCHER_PATH = USER_PATH + "/.config/ulauncher/user-themes"
+
+type Option int
+
+const (
+	ICONS Option = iota + 1
+	THEMES
+	CONFIG
+)
+
+func (option Option) String() string {
+	switch option {
+	case ICONS:
+		return ".icons"
+	case THEMES:
+		return ".themes"
+	case CONFIG:
+		return ".config"
+	default:
+		return ""
+	}
+}
+
+type Command struct {
+	Type    string
+	Option  Option
+	Options []string
+}
+
+func MenuSelection() {
+	var command Command
+	accessible, _ := strconv.ParseBool(os.Getenv("ACCESSIBLE"))
+
+	packages := GetPackages("config")
+
+	options := []huh.Option[string]{}
+	for index := 0; index < len(packages); index++ {
+		option := huh.Option[string]{}
+		option.Key = packages[index]
+		option.Value = packages[index]
+		option.Selected(false)
+		options = append(options, option)
+	}
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Options(huh.NewOptions(ICONS.String(), THEMES.String(), CONFIG.String())...).
+				Title("Linux Themes Remover").
+				Value(&command.Type),
+		),
+
+		huh.NewGroup(
+			huh.NewMultiSelect[string]().
+				Options(options...).
+				Title("Installed Themes:\n").
+				Value(&command.Options),
+		),
+	).WithAccessible(accessible).WithTheme(huh.ThemeDracula())
+	err := form.Run()
+	if err != nil {
+		fmt.Println("Uh oh:", err)
+		os.Exit(1)
+	}
+
+	prepareBurger := func() {
+		time.Sleep(3 * time.Second)
+	}
+	_ = spinner.New().
+		Title("Removing Themes...").
+		// TitleStyle(lipgloss.NewStyle().Foreground(green)).
+		Style(lipgloss.NewStyle().Foreground(red)).
+		Accessible(accessible).
+		Action(prepareBurger).
+		Run()
+
+		// Print results
+	fmt.Println(lipgloss.NewStyle().Foreground(green).SetString("Themes removed: list"))
+}
+
+func GetPackages(category string) []string {
+
+	var path string
+	if category == "icons" {
+		path = ICON_PATH
+	}
+	if category == "themes" {
+		path = THEME_PATH
+	}
+	if category == "config" {
+		path = CONFIG_PATH
+
+	}
+
+	packages := []string{}
+	files, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			packages = append(packages, path+"/"+file.Name())
+		}
+	}
+	return packages
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"main/components"
 	"os"
 	"os/exec"
@@ -242,13 +241,25 @@ func install(links []string, directory string) {
 
 func set_command() {
 	InDevelopment()
-	// gnome
-	// cinnamon
-	// i3
-	// wayland
 }
 
-// exists returns whether the given file or directory exists
+func remove_command() {
+	components.MenuSelection()
+}
+func Remove(packages []string, category string) {
+	for _, packag := range packages {
+		is_valid, err := Exists(packag)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if is_valid {
+			fmt.Println("directory to be removed: " + packag)
+			// if err := os.Remove(""); err != nil {
+			// 	fmt.Println(err.Error())
+			// }
+		}
+	}
+}
 func Exists(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err == nil {
@@ -259,181 +270,3 @@ func Exists(path string) (bool, error) {
 	}
 	return false, err
 }
-
-type Action int
-
-const (
-	Icons Action = iota
-	Themes
-	Config
-	Spin
-	Cancel
-)
-
-// var highlight = lipgloss.NewStyle().Foreground(lipgloss.Color("#00D7D7"))
-
-func remove_command() {
-
-	components.MenuSelection()
-
-	// spinnerStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("4"))
-
-	// theme := huh.ThemeBase16()
-	// theme.FieldSeparator = lipgloss.NewStyle().SetString("\n")
-	// theme.Help.FullKey.MarginTop(1)
-
-	// var action Action
-	// f := huh.NewForm(
-	// 	huh.NewGroup(
-	// 		huh.NewSelect[Action]().
-	// 			Value(&action).
-	// 			Options(
-	// 				huh.NewOption("Icons", Icons),
-	// 				huh.NewOption("Themes", Themes),
-	// 				huh.NewOption("Config", Config),
-	// 				huh.NewOption("Spinner", Spin),
-	// 				huh.NewOption("Cancel", Cancel),
-	// 			).
-	// 			Title("Choose Folder"),
-	// 	),
-	// ).WithTheme(theme)
-	// err := f.Run()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// var packages []string
-	// switch action {
-	// case Icons:
-	// 	fmt.Println("Icons Action")
-	// 	packages = GetPackages("icons")
-	// 	fmt.Println(packages)
-	// case Themes:
-	// 	fmt.Println("Themes Action")
-	// 	packages = GetPackages("themes")
-	// 	fmt.Println(packages)
-	// case Config:
-	// 	fmt.Println("Config")
-	// 	packages = GetPackages("config")
-	// 	fmt.Println(packages)
-	// case Spin:
-	// 	fmt.Println("Spinner Example")
-	// 	_ = spinner.New().Title("Spinner example...").Style(spinnerStyle).Run()
-	// case Cancel:
-	// 	fmt.Println("Cancelling...")
-	// 	os.Exit(1)
-	// }
-	// // fmt.Printf("Selected themes are: \n%s \n%s \n ", highlight.Render("theme_one"), highlight.Render("theme_two"))
-
-	// var nextAction string
-	// f = huh.NewForm(
-	// 	huh.NewGroup(
-	// 		huh.NewSelect[string]().
-	// 			Options(huh.NewOptions("packages")...).
-	// 			Title("What's next?").Value(&nextAction),
-	// 	),
-	// ).WithTheme(theme)
-
-	// err = f.Run()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if nextAction == "Remove" {
-	// 	_ = spinner.New().Title("Removing Packages: list...").Style(spinnerStyle).Run()
-	// 	fmt.Println("Packages Removed.")
-	// }
-
-}
-
-func GetPackages(category string) []string {
-
-	var path string
-	if category == "icons" {
-		path = ICON_PATH
-	}
-	if category == "themes" {
-		path = THEME_PATH
-	}
-	if category == "config" {
-		path = CONFIG_PATH
-
-	}
-
-	packages := []string{}
-	files, err := os.ReadDir(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, file := range files {
-		if file.IsDir() {
-			packages = append(packages, path+"/"+file.Name())
-		}
-	}
-	return packages
-}
-
-// func getThemeOptions() []string {}
-// func getThemeOptions() []string {}
-// func getThemeOptions() []string {}
-// func getThemeOptions() []string {}
-// func getThemeOptions() []string {}
-// func getThemeOptions() []string {}
-
-// func remove_command(options []string) {
-// 	fmt.Println("\n Removing... ")
-// 	if len(arguments) == 4 {
-// 		if _, err := Exists(arguments[3]); err != nil {
-// 			fmt.Println(err.Error())
-// 			panic(err)
-// 		}
-// 		if arguments[2] == "icons" {
-// 			icon_packs := []string{arguments[3]}
-// 			remove(icon_packs, ".icons")
-// 		}
-// 		if arguments[2] == "themes" {
-// 			themes_packs := []string{arguments[3]}
-// 			remove(themes_packs, ".themes")
-// 		}
-// 		if arguments[2] == "config" {
-// 			InDevelopment()
-// 			return
-// 		}
-// 		return
-// 	}
-
-// 	if len(arguments) > 3 {
-// 		file_path := arguments[3:]
-// 		directory := arguments[2]
-
-// 		fmt.Print("Packages: ")
-// 		fmt.Println(file_path)
-
-// 		packages := []string{}
-// 		for _, url := range file_path {
-// 			if !ValidUrl(url) {
-// 				help_command()
-// 				fmt.Println("Program End.")
-// 				syscall.Exit(0)
-// 			}
-// 			packages = append(packages, url)
-// 		}
-// 		install(packages, "."+directory)
-// 		return
-// 	}
-
-// 	help_command()
-// }
-
-// func remove(packages []string, directory string) {
-// 	for _, packag := range packages {
-// 		is_valid, err := Exists(packag)
-// 		if err != nil {
-// 			fmt.Println(err)
-// 		}
-// 		if is_valid {
-// 			fmt.Println("directory to be removed: " + packag)
-// 			// if err := os.Remove(""); err != nil {
-// 			// 	fmt.Println(err.Error())
-// 			// }
-// 		}
-// 	}
-// }
