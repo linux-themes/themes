b25f75130bfd0e479950786895323e529dac46cb
jordan <sudo.sendmail.jordan@gmail.com>
2024-05-20 15:38:03 -0700

    Fix: build test

--- cmd/themes.go
+++ cmd/themes.go
@@ -1,130 +1,82 @@
 package cmd
 
-import (
-	"github.com/charmbracelet/bubbles/help"
-	"github.com/charmbracelet/huh"
-	"github.com/charmbracelet/lipgloss"
-)
+// import (
+// 	_ "unsafe" // used for the hacky interalCopy linking.
 
-type CustomTheme huh.Theme
-type CustomFieldStyles huh.FieldStyles
-type CustomTextInputStyles huh.TextInputStyles
+// 	"github.com/charmbracelet/bubbles/help"
+// 	"github.com/charmbracelet/huh"
+// 	"github.com/charmbracelet/lipgloss"
+// )
 
-func (t CustomTheme) Copy() CustomTheme {
-	return CustomTheme{
-		Form:           t.Form.Copy(),
-		Group:          t.Group.Copy(),
-		FieldSeparator: t.FieldSeparator.Copy(),
-		Blurred:        t.Blurred.Copy(),
-		Focused:        t.Focused.Copy(),
-		Help: help.Styles{
-			Ellipsis:       t.Help.Ellipsis.Copy(),
-			ShortKey:       t.Help.ShortKey.Copy(),
-			ShortDesc:      t.Help.ShortDesc.Copy(),
-			ShortSeparator: t.Help.ShortSeparator.Copy(),
-			FullKey:        t.Help.FullKey.Copy(),
-			FullDesc:       t.Help.FullDesc.Copy(),
-			FullSeparator:  t.Help.FullSeparator.Copy(),
-		},
-	}
-}
+// // make copy public
 
-func (t CustomTextInputStyles) Copy() CustomTextInputStyles {
-	return CustomTextInputStyles{
-		Cursor:      t.Cursor.Copy(),
-		Placeholder: t.Placeholder.Copy(),
-		Prompt:      t.Prompt.Copy(),
-		Text:        t.Text.Copy(),
-	}
-}
+// // maybe a bit hacky but the copy method isn't exported for some reason because its private duh
+// //
+// //go:linkname Copy github.com/charmbracelet/huh.Theme.copy
+// func Copy(huh.Theme) huh.Theme
 
-func (f CustomFieldStyles) Copy() CustomFieldStyles {
-	return CustomFieldStyles{
-		Base:                f.Base.Copy(),
-		Title:               f.Title.Copy(),
-		Description:         f.Description.Copy(),
-		ErrorIndicator:      f.ErrorIndicator.Copy(),
-		ErrorMessage:        f.ErrorMessage.Copy(),
-		SelectSelector:      f.SelectSelector.Copy(),
-		Option:              f.Option.Copy(),
-		MultiSelectSelector: f.MultiSelectSelector.Copy(),
-		SelectedOption:      f.SelectedOption.Copy(),
-		SelectedPrefix:      f.SelectedPrefix.Copy(),
-		UnselectedOption:    f.UnselectedOption.Copy(),
-		UnselectedPrefix:    f.UnselectedPrefix.Copy(),
-		FocusedButton:       f.FocusedButton.Copy(),
-		BlurredButton:       f.BlurredButton.Copy(),
-		TextInput:           f.TextInput.Copy(),
-		Card:                f.Card.Copy(),
-		NoteTitle:           f.NoteTitle.Copy(),
-		Next:                f.Next.Copy(),
-	}
-}
+// func ThemeBase() *huh.Theme {
+// 	var t huh.Theme
 
-// make copy public
+// 	t.FieldSeparator = lipgloss.NewStyle()
+// 	button := lipgloss.NewStyle()
 
-func ThemeBase() *huh.Theme {
-	var t huh.Theme
+// 	f := &t.Focused
+// 	f.Base = lipgloss.NewStyle()
+// 	f.Card = lipgloss.NewStyle()
+// 	f.ErrorIndicator = lipgloss.NewStyle().SetString(" *")
+// 	f.ErrorMessage = lipgloss.NewStyle().SetString(" *")
+// 	f.SelectSelector = lipgloss.NewStyle().SetString("> ")
+// 	f.MultiSelectSelector = lipgloss.NewStyle().SetString("> ")
+// 	f.SelectedPrefix = lipgloss.NewStyle().SetString("[•] ")
+// 	f.UnselectedPrefix = lipgloss.NewStyle().SetString("[ ] ")
+// 	f.FocusedButton = button.Copy().Foreground(lipgloss.Color("0")).Background(lipgloss.Color("7"))
+// 	f.BlurredButton = button.Copy().Foreground(lipgloss.Color("7")).Background(lipgloss.Color("0"))
+// 	f.TextInput.Placeholder = lipgloss.NewStyle().Foreground(lipgloss.Color("8"))
 
-	t.FieldSeparator = lipgloss.NewStyle()
-	button := lipgloss.NewStyle()
+// 	t.Help = help.New().Styles
+// 	t.Blurred = f.Copy()
+// 	t.Blurred.Base = t.Blurred.Base.BorderStyle(lipgloss.HiddenBorder())
+// 	t.Blurred.MultiSelectSelector = lipgloss.NewStyle().SetString("  ")
 
-	f := &t.Focused
-	f.Base = lipgloss.NewStyle()
-	f.Card = lipgloss.NewStyle()
-	f.ErrorIndicator = lipgloss.NewStyle().SetString(" *")
-	f.ErrorMessage = lipgloss.NewStyle().SetString(" *")
-	f.SelectSelector = lipgloss.NewStyle().SetString("> ")
-	f.MultiSelectSelector = lipgloss.NewStyle().SetString("> ")
-	f.SelectedPrefix = lipgloss.NewStyle().SetString("[•] ")
-	f.UnselectedPrefix = lipgloss.NewStyle().SetString("[ ] ")
-	f.FocusedButton = button.Copy().Foreground(lipgloss.Color("0")).Background(lipgloss.Color("7"))
-	f.BlurredButton = button.Copy().Foreground(lipgloss.Color("7")).Background(lipgloss.Color("0"))
-	f.TextInput.Placeholder = lipgloss.NewStyle().Foreground(lipgloss.Color("8"))
+// 	return &t
+// }
 
-	t.Help = help.New().Styles
-	t.Blurred = f.Copy()
-	t.Blurred.Base = t.Blurred.Base.BorderStyle(lipgloss.HiddenBorder())
-	t.Blurred.MultiSelectSelector = lipgloss.NewStyle().SetString("  ")
+// func ThemeCustom() *huh.Theme {
+// 	t := ThemeBase().Copy()
 
-	return &t
-}
+// 	var (
+// 		normalFg = lipgloss.AdaptiveColor{Light: "235", Dark: "252"}
+// 		indigo   = lipgloss.AdaptiveColor{Light: "#5A56E0", Dark: "#7571F9"}
+// 		cream    = lipgloss.AdaptiveColor{Light: "#FFFDF5", Dark: "#FFFDF5"}
+// 		fuchsia  = lipgloss.Color("#F780E2")
+// 		green    = lipgloss.AdaptiveColor{Light: "#02BA84", Dark: "#02BF87"}
+// 		red      = lipgloss.AdaptiveColor{Light: "#FF4672", Dark: "#ED567A"}
+// 	)
 
-func ThemeCustom() *huh.Theme {
-	t := ThemeBase().Copy()
+// 	f := &t.Focused
+// 	f.Base = lipgloss.NewStyle().
+// 		PaddingLeft(1)
+// 	f.Title.Foreground(green).Bold(true)
+// 	f.NoteTitle.Foreground(indigo).Bold(true).MarginBottom(1)
+// 	f.Description.Foreground(lipgloss.AdaptiveColor{Light: "", Dark: "243"})
+// 	f.ErrorIndicator.Foreground(red)
+// 	f.ErrorMessage.Foreground(red)
+// 	f.SelectSelector.Foreground(red)
+// 	f.Option.Foreground(normalFg)
+// 	f.MultiSelectSelector.Foreground(red)
+// 	f.SelectedOption.Foreground(lipgloss.ANSIColor(11))
+// 	f.SelectedPrefix = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#02CF92", Dark: "#02A877"}).SetString("✓ ")
+// 	f.UnselectedPrefix = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "", Dark: "243"}).SetString("• ")
+// 	f.UnselectedOption.Foreground(normalFg)
+// 	f.FocusedButton.Foreground(cream).Background(fuchsia)
+// 	f.Next = f.FocusedButton.Copy()
+// 	f.BlurredButton.Foreground(normalFg).Background(lipgloss.AdaptiveColor{Light: "252", Dark: "237"})
+// 	f.TextInput.Cursor.Foreground(green)
+// 	f.TextInput.Placeholder.Foreground(lipgloss.AdaptiveColor{Light: "248", Dark: "238"})
+// 	f.TextInput.Prompt.Foreground(fuchsia)
 
-	var (
-		normalFg = lipgloss.AdaptiveColor{Light: "235", Dark: "252"}
-		indigo   = lipgloss.AdaptiveColor{Light: "#5A56E0", Dark: "#7571F9"}
-		cream    = lipgloss.AdaptiveColor{Light: "#FFFDF5", Dark: "#FFFDF5"}
-		fuchsia  = lipgloss.Color("#F780E2")
-		green    = lipgloss.AdaptiveColor{Light: "#02BA84", Dark: "#02BF87"}
-		red      = lipgloss.AdaptiveColor{Light: "#FF4672", Dark: "#ED567A"}
-	)
+// 	t.Blurred = f.Copy()
 
-	f := &t.Focused
-	f.Base = lipgloss.NewStyle().
-		PaddingLeft(1)
-	f.Title.Foreground(green).Bold(true)
-	f.NoteTitle.Foreground(indigo).Bold(true).MarginBottom(1)
-	f.Description.Foreground(lipgloss.AdaptiveColor{Light: "", Dark: "243"})
-	f.ErrorIndicator.Foreground(red)
-	f.ErrorMessage.Foreground(red)
-	f.SelectSelector.Foreground(red)
-	f.Option.Foreground(normalFg)
-	f.MultiSelectSelector.Foreground(red)
-	f.SelectedOption.Foreground(lipgloss.ANSIColor(11))
-	f.SelectedPrefix = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#02CF92", Dark: "#02A877"}).SetString("✓ ")
-	f.UnselectedPrefix = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "", Dark: "243"}).SetString("• ")
-	f.UnselectedOption.Foreground(normalFg)
-	f.FocusedButton.Foreground(cream).Background(fuchsia)
-	f.Next = f.FocusedButton.Copy()
-	f.BlurredButton.Foreground(normalFg).Background(lipgloss.AdaptiveColor{Light: "252", Dark: "237"})
-	f.TextInput.Cursor.Foreground(green)
-	f.TextInput.Placeholder.Foreground(lipgloss.AdaptiveColor{Light: "248", Dark: "238"})
-	f.TextInput.Prompt.Foreground(fuchsia)
-
-	t.Blurred = f.Copy()
-
-	return &t
-}
+// 	return &t
+// }
