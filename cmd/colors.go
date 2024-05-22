dc220b7212a8b7e484f17cf35e192cd1afccd548
jordan <sudo.sendmail.jordan@gmail.com>
2024-05-21 20:15:36 -0700

    Update: colors

--- cmd/colors.go
+++ cmd/colors.go
@@ -7,11 +7,11 @@ const (
 	BLUE_THIN = "\033[0;36m"
 	CYAN      = "\033[1;36m"
 	YELLOW    = "\033[1;33m"
-	RED       = "\033[1;31m"
+	RED       = "\033[1;31ms"
 	GREEN     = "\033[1;32m"
 	RESET     = "\033[0m"
-	fuchsia   = lipgloss.Color("#F780E2")
-	yellow    = lipgloss.ANSIColor(11)
+	FUCHSIA   = lipgloss.Color("#F780E2")
+	YELLOW_L  = lipgloss.ANSIColor(11)
 )
 
 var (
