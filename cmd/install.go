3f740d47b278c96e36078e7a0a20b2b3eb0752d5
jordan <sudo.sendmail.jordan@gmail.com>
2024-10-06 20:05:16 -0700

    Update install.go

--- cmd/install.go
+++ cmd/install.go
@@ -21,7 +21,7 @@ var offical_icons = map[int]string{
 	4: "https://github.com/linux-themes/themes-official/raw/main/icons/WhiteSur.tar.xz",
 	5: "https://github.com/linux-themes/themes-official/raw/main/icons/Win10Sur.tar.xz",
 	6: "https://github.com/linux-themes/themes-official/raw/main/icons/Win11.tar.xz",
-	7: "https://github.com/linux-themes/themes-official/raw/main/icons/mint.tar.xz",
+	7: "https://github.com/linux-themes/themes-official/raw/main/icons/Mint.tar.xz",
 }
 var offical_themes = map[int]string{
 	1: "https://github.com/linux-themes/themes-official/raw/main/themes/gnome/MarbleShell.tar.gz",
